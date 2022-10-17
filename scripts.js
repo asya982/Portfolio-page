06dd2493f8f4a2d4d19017c8a95e2a7bbf727aa9
Anastasiia <bagin.nst@gmail.com>
2022-10-17 16:37:23 +0300

    final result

---
+++ scripts.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 0; i < projects.length; i++) {
+        document.getElementsByClassName("project-grid")[0].insertAdjacentHTML('beforeend', 
+        `<a href="${projects[i].url}" class="project" target="_blank">
+            <img src="${projects[i].img}" alt="${projects[i].title}">
+            <p class="project-title">
+                <span class="code"><</span>
+                ${projects[i].title}
+                <span class="code">/></span>
+            </p>
+        </a>` );
+    }
+})
---
+++ static/projects.js
@@ -0,0 +1,38 @@
+let projects = [
+    {
+        "id": 1,
+        "title" : "Movie4Night",
+        "img" : "./static/images/Movie.png",
+        "url" : "https://movie4night.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "title" : "Nft preview card ",
+        "img" : "./static/images/nft.png",
+        "url" : "https://asya982.github.io/Nft-preview-card/"
+    },
+    {
+        "id": 3,
+        "title" : "Interactive rating",
+        "img" : "./static/images/rating.png",
+        "url" : "https://asya982.github.io/interactive-rating-component/"
+    },
+    {
+        "id": 4,
+        "title" : "Product preview card",
+        "img" : "./static/images/card-product.png",
+        "url" : "https://asya982.github.io/Product-preview-card-component/"
+    },
+    {
+        "id": 5,
+        "title" : "Wes Anderson Tribute Page",
+        "img" : "./static/images/Wes-Anderson.png",
+        "url" : "https://asya982.github.io/Wes-Anderson-Tribute-Page/"
+    },
+    {
+        "id": 6,
+        "title" : "Tribute page",
+        "img" : "./static/images/tribute.png",
+        "url" : "https://movie4night.herokuapp.com/"
+    }
+]
