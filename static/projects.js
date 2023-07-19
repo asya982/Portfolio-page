3eb68eae9f93647acbd40d60e86f814fa27f10e0
Asya <bagin.nst@gmail.com>
2023-07-19 14:01:33 +0300

    fix: /static updated projects and links

--- static/projects.js
+++ static/projects.js
@@ -1,38 +1,38 @@
-let projects = [
-    {
-        "id": 1,
-        "title" : "Movie4Night",
-        "img" : "./static/images/Movie.png",
-        "url" : "https://movie4night.herokuapp.com/"
-    },
-    {
-        "id": 2,
-        "title" : "Nft preview card ",
-        "img" : "./static/images/nft.png",
-        "url" : "https://asya982.github.io/Nft-preview-card/"
-    },
-    {
-        "id": 3,
-        "title" : "Interactive rating",
-        "img" : "./static/images/rating.png",
-        "url" : "https://asya982.github.io/interactive-rating-component/"
-    },
-    {
-        "id": 4,
-        "title" : "Product preview card",
-        "img" : "./static/images/card-product.png",
-        "url" : "https://asya982.github.io/Product-preview-card-component/"
-    },
-    {
-        "id": 5,
-        "title" : "Wes Anderson Tribute Page",
-        "img" : "./static/images/Wes-Anderson.png",
-        "url" : "https://asya982.github.io/Wes-Anderson-Tribute-Page/"
-    },
-    {
-        "id": 6,
-        "title" : "Tribute page",
-        "img" : "./static/images/tribute.png",
-        "url" : "https://movie4night.herokuapp.com/"
-    }
-]
+const projects = [
+  {
+    id: 1,
+    title: "UpTalent",
+    img: "./static/images/Uptalent.jpg",
+    url: "https://white-plant-071773303.3.azurestaticapps.net/home",
+  },
+  {
+    id: 2,
+    title: "Movie4Night",
+    img: "./static/images/Movie.png",
+    url: "https://movie4night.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Wes Anderson Tribute Page",
+    img: "./static/images/Wes-Anderson.png",
+    url: "https://asya982.github.io/Wes-Anderson-Tribute-Page/",
+  },
+  {
+    id: 4,
+    title: "Interactive rating",
+    img: "./static/images/rating.png",
+    url: "https://asya982.github.io/interactive-rating-component/",
+  },
+  {
+    id: 5,
+    title: "Product preview card",
+    img: "./static/images/card-product.png",
+    url: "https://asya982.github.io/Product-preview-card-component/",
+  },
+  {
+    id: 6,
+    title: "ES6 Smartphone",
+    img: "./static/images/Smartphone.png",
+    url: "https://asya982.github.io/JS-Course-IndividualWork/",
+  },
+];
